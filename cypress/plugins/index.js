e37492ad3a27bdce62b57c8de964389e22c5515c
Paulius <paulius.michelevicius@gmail.com>
2023-06-12 07:33:21 +0100

    VER-3: depencies update and cypress removal.

--- cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-const injectDevServer = require('@cypress/react/plugins/react-scripts')
-
-module.exports = (on, config) => {
-  injectDevServer(on, config)
-
-  config.env.customVariable = process.env.CUSTOM_VAR || 'Example'
-
-  return config
-}
